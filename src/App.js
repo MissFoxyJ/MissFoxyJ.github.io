f5f4a17e5d807eed60b7f7109281d4efa498afe0
MissFoxyJ <56697413+MissFoxyJ@users.noreply.github.com>
2023-09-20 14:16:31 +0000

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <LoadingSpinner />
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
